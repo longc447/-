4ea90c0f4183bf1f12650a37e8ebf8c5eb44705d
longc447 <longc447@gmail.com>
2022-06-06 01:08:53 +0800

    U:配置文件调整，针对多个平台的不同配置问题

--- common/js/config.js
+++ common/js/config.js
@@ -1,40 +1,25 @@
-// import A from ''
+import {ApiServer,useSetting} from '@/edition'
+console.log(ApiServer,useSetting,"ApiServer,useSetting")
 /**
  * 镜片一站式采购平台
  */
-const shuming = 'https://shuming.tongzhounetwork.ltd'
-const caocao = 'https://caocao.tongzhounetwork.ltd'
-const yunyi = 'https://chengxu.tongzhounetwork.ltd'
-const Api1168 = 'https://1168.tongzhounetwork.ltd'
-const tianbo = 'https://tianbo.tongzhounetwork.ltd'
-const setting = {
-	tianbo:{
-		is_wholesaler: 3,
-		isMustLogin: 0, 
-	}
-}
-const ApiServer = tianbo;
-const useSetting = setting.tianbo;
 const x = {
-	// baseUrl: 'https://qinghe.tongzhounetwork.ltd',
-	
 	baseUrl: ApiServer,
 	imgDomain: ApiServer,
 	h5Domain: ApiServer+'/h5',
 	mpKey: 'D5YBZ-ZWPWU-SAFVB-BQBT4-Q2A56-LJFRH',
 	//小程序appid wx0f57e059962fab6a
 }
+
 const globalConfig = {
 	// 价格保护 3是无保护 0是有保护
 	is_wholesaler: 3,
 	// 是否强制登陆 0不强制 1强制
-	isMustLogin: 1, 
+	isMustLogin: 0, 
 	// 是否需要验证批发商 0不需要 1强制
-	isPfs:1,
+	isPfs:0,
 	//  是否显示销量 0关闭 1显示
 	isOpenVirtualSale:0,
-	// 是否开启定位功能 0关闭 1开启
-	// isEnableLocation:0
 	...useSetting
 }
 let param = {
---
+++ edition.js
@@ -0,0 +1,25 @@
+const shuming = 'https://shuming.tongzhounetwork.ltd'
+const caocao = 'https://caocao.tongzhounetwork.ltd'
+const yunyi = 'https://chengxu.tongzhounetwork.ltd'
+const Api1168 = 'https://1168.tongzhounetwork.ltd'
+const tianbo = 'https://tianbo.tongzhounetwork.ltd'
+const setting = {
+	//天铂眼镜
+	tianbo: {
+		is_wholesaler: 3,
+		isMustLogin: 0,
+	},
+	caocao: {
+		// 价格保护 3是无保护 0是有保护
+		is_wholesaler: 3,
+		// 是否强制登陆 0不强制 1强制
+		isMustLogin: 0,
+		// 是否需要验证批发商 0不需要 1强制
+		isPfs: 0,
+		//  是否显示销量 0关闭 1显示
+		isOpenVirtualSale: 1,
+	}
+}
+
+export const ApiServer = caocao;
+export const useSetting = setting.caocao;
